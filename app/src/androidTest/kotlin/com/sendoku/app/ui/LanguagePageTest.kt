d80625e6e35ad6c5dd9de15637c07458f2027b9f
adilzhanY <adilzhan1112@gmail.com>
2026-09-08 14:25:43 +0200

    scroll to the language row before checking it is on screen

--- app/src/androidTest/kotlin/com/sendoku/app/ui/LanguagePageTest.kt
+++ app/src/androidTest/kotlin/com/sendoku/app/ui/LanguagePageTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
 import com.sendoku.app.data.Appearance
@@ -83,7 +84,7 @@ class LanguagePageTest {
             1,
             compose.onAllNodesWithText("Language").fetchSemanticsNodes().size,
         )
-        compose.onAllNodesWithText("English")[0].assertIsDisplayed()
+        compose.onNodeWithText("English").performScrollTo().assertIsDisplayed()
     }
 
     @Test
